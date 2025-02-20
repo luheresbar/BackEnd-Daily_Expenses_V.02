c7eaf180a3f90b7bb27f59734b676e442f964425
luheresbar <luheresbar@gmail.com>
2025-02-20 09:47:13 -0500

    first commit

---
+++ src/main/java/com/daily_expenses/DailyExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.daily_expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyExpensesApplication.class, args);
+	}
+
+}
